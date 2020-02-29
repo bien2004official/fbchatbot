7206c71fa181288046594b1a166ca49f5439c566
Bien <bien2004official@gmail.com>
2020-02-29 08:24:21 +0700

    fixed something fucked converter

--- messenger_bot/modules/base64_converter.py
+++ messenger_bot/modules/base64_converter.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import base64
 
 def converter(password):
@@ -7,13 +6,3 @@ def converter(password):
     message_bytes = base64.b64decode(base64_bytes)
     password = message_bytes.decode('ascii')
     return password;
-=======
-import base64
-
-def converter(password):
-    base64_message = password
-    base64_bytes = base64_message.encode('ascii')
-    message_bytes = base64.b64decode(base64_bytes)
-    password = message_bytes.decode('ascii')
-    return password;
->>>>>>> 6b6d7adb40c0e610ca6a76cde42f6fd29bfc6b45
